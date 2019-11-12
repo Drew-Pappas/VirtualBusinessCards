6b7c011f72b87f35251a3aa0c406a3fac3e79acf
Drew <drewpap@umich.edu>
2019-11-12 10:35:31 -0500

    Added activity structure

---
+++ app/src/main/java/com/example/virtualbusinesscards/myProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.virtualbusinesscards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
