ef5b637f2d3a3f746c7ee405b58833b4a49f3b3b
Drew <drewpap@umich.edu>
2019-11-14 09:34:55 -0500

    Commit test

--- app/src/main/java/com/example/virtualbusinesscards/contactsActivity.java
+++ app/src/main/java/com/example/virtualbusinesscards/contactsActivity.java
@@ -11,5 +11,5 @@ public class contactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
     }
-    //Commmit test
+    //Test Commit
 }
