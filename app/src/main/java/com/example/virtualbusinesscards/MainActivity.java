fa156a65e7ddcfafe0dfad80edd1dea09f146aa1
hatu kanu <hatu@umich.edu>
2019-11-21 09:53:32 -0500

    Main activity declaration and connection

--- app/src/main/java/com/example/virtualbusinesscards/MainActivity.java
+++ app/src/main/java/com/example/virtualbusinesscards/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.virtualbusinesscards;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare objects
+    Button buttonSignUpMain, buttonLoginMain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //helloworld
+
+        //connect to UI
+        buttonSignUpMain = findViewById(R.id.buttonSignUpMain);
+        buttonLoginMain = findViewById(R.id.buttonLogInMain);
+
+        //make sure buttons are working 
     }
 }
