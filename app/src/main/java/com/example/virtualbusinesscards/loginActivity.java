6eb3a4671d3167b0935754f06f4634a8f1c9ce41
Drew <drewpap@umich.edu>
2019-11-27 14:22:52 -0500

    Added Firebase authentication to sign up and log in activities Added validation for matching passwords Added phone number validation

--- app/src/main/java/com/example/virtualbusinesscards/loginActivity.java
+++ app/src/main/java/com/example/virtualbusinesscards/loginActivity.java
@@ -1,13 +1,21 @@
 package com.example.virtualbusinesscards;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 // Testing commit
 // Testing commit without update
@@ -15,7 +23,8 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
 
     // declare objects
     EditText editTextEmailLogin, editTextPasswordLogin;
-    Button buttonLoginLogin, buttonForgot;
+    Button buttonLoginLogin, buttonForgot, buttonLoginBack;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -25,15 +34,17 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
 
         // connect to Ui
         editTextEmailLogin = findViewById(R.id.editTextEmailLogin);
-        editTextPasswordLogin = findViewById(R.id.editTextEmailLogin);
+        editTextPasswordLogin = findViewById(R.id.editTextPasswordLogin);
         buttonLoginLogin = findViewById(R.id.buttonLoginLogIn);
         buttonForgot = findViewById(R.id.buttonForgot);
+        buttonLoginBack = findViewById(R.id.buttonLoginBack);
 
         //make sure buttons are working
         buttonLoginLogin.setOnClickListener(this);
         buttonForgot.setOnClickListener(this);
+        buttonLoginBack.setOnClickListener(this);
 
-
+        mAuth = FirebaseAuth.getInstance();
 
 
     }
@@ -41,9 +52,37 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
 
-        if (view == buttonLoginLogin){
-            Intent homepageIntent = new Intent(loginActivity.this, homePageActivity.class);
-            startActivity(homepageIntent);
+        String email = editTextEmailLogin.getText().toString();
+        String password = editTextPasswordLogin.getText().toString();
+
+        if (view == buttonLoginLogin && (!email.equals("") && !password.equals(""))){
+
+            mAuth.signInWithEmailAndPassword(email, password) //Handles signing in users
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, go to portal activity
+                                Intent homepageIntent = new Intent(loginActivity.this, homePageActivity.class);
+                                startActivity(homepageIntent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(loginActivity.this, "Log in failed", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+
+        } else if (view == buttonLoginLogin && (email.equals("") || password.equals(""))){
+            Toast.makeText(this, "Enter your email and password", Toast.LENGTH_SHORT).show();
+        }
+
+        if (view == buttonLoginBack){
+            Intent mainActivityIntent = new Intent(loginActivity.this, MainActivity.class);
+            startActivity(mainActivityIntent);
         }
 
     }
--- app/src/main/java/com/example/virtualbusinesscards/signUpActivity.java
+++ app/src/main/java/com/example/virtualbusinesscards/signUpActivity.java
@@ -1,20 +1,116 @@
 package com.example.virtualbusinesscards;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText editTextSignUpFirstName, editTextSignUpLastName, editTextSignUpPhoneNumber,
+             editTextSignUpEmail, editTextSignUpPassword, editTextSignUpReEnterPassword;
+
+    Button buttonSignUpBack, buttonSignUpComplete;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        editTextSignUpFirstName = findViewById(R.id.editTextSignUpFirstName);
+        editTextSignUpLastName = findViewById(R.id.editTextSignUpLastName);
+        editTextSignUpPhoneNumber = findViewById(R.id.editTextSignUpPhoneNumber);
+        editTextSignUpEmail = findViewById(R.id.editTextSignUpEmail);
+        editTextSignUpPassword = findViewById(R.id.editTextSignUpPassword);
+        editTextSignUpReEnterPassword = findViewById(R.id.editTextSignUpReEnterPassword);
+
+        buttonSignUpBack = findViewById(R.id.buttonSignUpBack);
+        buttonSignUpComplete = findViewById(R.id.buttonSignUpComplete);
+
+        buttonSignUpBack.setOnClickListener(this);
+        buttonSignUpComplete.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
     public void onClick(View view) {
+        if (view == buttonSignUpComplete) {
+            String name = editTextSignUpFirstName.getText().toString()
+                    + editTextSignUpLastName.getText().toString();
+            String phoneNumber = editTextSignUpPhoneNumber.getText().toString()
+                    .replaceAll("\\s","")
+                    .replaceFirst("(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3");
+            String email = editTextSignUpEmail.getText().toString();
+            String password = editTextSignUpPassword.getText().toString();
+            String passwordConfirm = editTextSignUpReEnterPassword.getText().toString();
 
+            if (!email.equals("")
+                    && !password.equals("")
+                    && !name.equals("")
+                    && !phoneNumber.equals("")
+                    && !passwordConfirm.equals("")){
+
+                if (!password.equals(passwordConfirm)){
+                    editTextSignUpPassword.setError("Passwords do not match");
+                    editTextSignUpReEnterPassword.setError("Passwords do not match");
+                }
+
+                //Check the formatting of a phone number
+                if (phoneNumber.length() != 12){
+                    editTextSignUpPhoneNumber.setError("Phone number must be ten digits");
+
+                }
+
+                if (password.equals(passwordConfirm) && phoneNumber.length() == 12){
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(signUpActivity.this,
+                                                "Account successfully created", Toast.LENGTH_SHORT).show();
+
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Intent loginIntent = new Intent(signUpActivity.this, loginActivity.class);
+                                        startActivity(loginIntent);
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(signUpActivity.this, "Failed to create account",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+
+
+
+
+            } else {
+                Toast.makeText(this, "All fields must be entered", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
     }
 }
